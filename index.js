035799ae41f53874eba1176b60cf934429d8ad34
Julian Teixido <juliteixido12@gmail.com>
2026-05-06 16:03:40 -0300

    Support GOOGLE_CREDENTIALS_B64 env var for Railway deploy

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 'use strict';
 
 require('dotenv').config();
+const fs = require('fs');
+
+// En Railway, credentials.json se pasa como base64 en GOOGLE_CREDENTIALS_B64
+if (process.env.GOOGLE_CREDENTIALS_B64 && !fs.existsSync('./credentials.json')) {
+  fs.writeFileSync('./credentials.json', Buffer.from(process.env.GOOGLE_CREDENTIALS_B64, 'base64').toString('utf8'));
+  console.log('[Init] credentials.json generado desde variable de entorno.');
+}
+
 const { startBot } = require('./src/bot');
 
 startBot().catch((err) => {
